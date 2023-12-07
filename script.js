16e19886554191f6e9e36de87c2673494bd5cee2
sep.vaz <g12sep@gmail.com>
2023-12-07 12:41:52 +0100

    lite fix o trix

--- script.js
+++ script.js
@@ -9,75 +9,80 @@ const storedPlayerName = localStorage.getItem("playerName");
 
 if (storedPlayerName) {
   displayPlayerName(storedPlayerName);
-} 
-
-function displayPlayerName(playerName) {
-const playerNameElement = document.getElementById("playerNameDisplay");
-
-if (playerNameElement) {
-    playerNameElement.innerText = "Welcome, " + playerName + "!";
-} 
 }
 
-rockBtn.addEventListener("click", function() {
-    playGame("Rock")
+function displayPlayerName(playerName) {
+  const playerNameElement = document.getElementById("playerNameDisplay");
+
+  if (playerNameElement) {
+    playerNameElement.innerText = "Welcome, " + playerName + "!";
+  }
+}
+
+
+rockBtn.addEventListener("click", function () {
+  playGame("Rock")
 });
 
-paperBtn.addEventListener("click", function() {
-    playGame("Paper")
+paperBtn.addEventListener("click", function () {
+  playGame("Paper")
 });
 
-scissorsBtn.addEventListener("click", function() {
-    playGame("Scissors")});
+scissorsBtn.addEventListener("click", function () {
+  playGame("Scissors")
+});
+
 
 
 function playGame(playerChoice) {
-    const choices = ['Rock', 'Paper', 'Scissors'];
-    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+  const choices = ['Rock', 'Paper', 'Scissors'];
+  const computerChoice = choices[Math.floor(Math.random() * choices.length)];
 
-    document.getElementById('playerChoice').innerText = ' ' + playerChoice;
-    document.getElementById('computerChoice').innerText = computerChoice;
+  document.getElementById('playerChoice').innerText = ' ' + playerChoice;
+  document.getElementById('computerChoice').innerText = computerChoice;
 
-    const result = determineWinner(playerChoice, computerChoice);
+  const result = determineWinner(playerChoice, computerChoice);
 
-    document.getElementById('result').innerText = result;
+  document.getElementById('result').innerText = result;
 
-    if (result === "It's a tie!" || result === storedPlayerName + ' wins!' || result === 'Computer wins!') {
-      let resultElement = document.getElementById("result")
-      resultElement.classList.remove("winner-animation");
-      resultElement.classList.add('winner-animation');
-      setTimeout(() => {
-          resultElement.classList.remove('winner-animation');
-      }, 1000);
+  if (result === "It's a tie!" || result === ' You win!' || result === 'Computer wins!') {
+    let resultElement = document.getElementById("result")
+    resultElement.classList.remove("winner-animation");
+    resultElement.classList.add('winner-animation');
+    setTimeout(() => {
+      resultElement.classList.remove('winner-animation');
+    }, 1000);
   }
-  }
-
-
-  function determineWinner(playerChoice, computerChoice) {
-    const playerName = playerNameInput.value;
-    if (playerChoice === computerChoice) {
-      return  "It's a tie!";
-    } else if (
-      (playerChoice === 'Rock' && computerChoice === 'Scissors') ||
-      (playerChoice === 'Paper' && computerChoice === 'Rock') ||
-      (playerChoice === 'Scissors' && computerChoice === 'Paper')
-    ) {
-      return storedPlayerName + ' wins!';
-    } else {
-      return 'Computer wins!';
-    }
-  }
-  
-
-  function resetGame() {
-    document.getElementById("playerChoice").innerText = "";
-    document.getElementById("computerChoice").innerText = "";
-    document.getElementById("result").innerText = "";
 }
 
-resetBtn.addEventListener("click", function() {
-    resetGame();
+
+function determineWinner(playerChoice, computerChoice) {
+  const playerName = playerNameInput.value;
+  if (playerChoice === computerChoice) {
+    return "It's a tie!";
+  } else if (
+    (playerChoice === 'Rock' && computerChoice === 'Scissors') ||
+    (playerChoice === 'Paper' && computerChoice === 'Rock') ||
+    (playerChoice === 'Scissors' && computerChoice === 'Paper')
+  ) {
+    return ' You win!';
+  } else {
+    return 'Computer wins!';
+  }
+}
+
+
+function resetGame() {
+  document.getElementById("playerChoice").innerText = "";
+  document.getElementById("computerChoice").innerText = "";
+  document.getElementById("result").innerText = "";
+}
+
+resetBtn.addEventListener("click", function () {
+  resetGame();
 });
+
+
 
 function backHome() {
   window.location.href = "start.html"
@@ -85,7 +90,7 @@ function backHome() {
 
 const backBtn = document.getElementById("backBtn")
 
-backBtn.addEventListener("click", function() {
+backBtn.addEventListener("click", function () {
   backHome()
 })
 
--- start.js
+++ start.js
@@ -1,35 +1,31 @@
 
-const form =  document.getElementById("playerForm")
+const form = document.getElementById("playerForm")
 
-function submitForm(event){
+function submitForm(event) {
     event.preventDefault();
-  } 
-  
-  form.addEventListener('submit', submitForm);
+}
+
+form.addEventListener('submit', submitForm);
 
 const playerNameInput = document.getElementById("playerName");
 
-document.addEventListener('DOMContentLoaded', function() {
-    const playerNameInput = document.getElementById("playerName");
-    const startBtn = document.getElementById("startBtn");
+startBtn.addEventListener("click", function () {
+    const playerName = playerNameInput.value.trim();
 
-    startBtn.addEventListener("click", function() {
-        const playerName = playerNameInput.value.trim();
+    if (playerName === "") {
+        alert("Please enter your name!");
+        return;
+    }
 
-        if (playerName === "") {
-            alert("Please enter your name!");
-            return;
-        }
+    localStorage.setItem("playerName", playerName);
 
-        localStorage.setItem("playerName", playerName);
-
-        window.location.href = "index.html";
-    });
+    window.location.href = "index.html";
 });
 
-playerNameInput.addEventListener("input", function() {
+
+playerNameInput.addEventListener("input", function () {
     let inputValue = playerNameInput.value;
     inputValue = inputValue.charAt(0).toUpperCase() + inputValue.slice(1);
     playerNameInput.value = inputValue;
-  });
-  
+});
+
